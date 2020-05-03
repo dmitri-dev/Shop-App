46d1ac94fa99aacbcca7effb9c77ceb9aa26d852
pinyana <60167177+pinyana@users.noreply.github.com>
2020-05-03 14:52:11 -0400

    State Management
    Provider / Listeners / class Products

--- main.dart
+++ main.dart
@@ -1,17 +1,45 @@
-import 'package:flappy_shop/screens/products_overview_screen.dart';
+import 'providers/products.dart';
+import 'screens/product_detail_screen.dart';
+import 'screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+        primaryColor: Color.fromRGBO(212, 175, 55, 1),
+          textTheme: TextTheme(
+            title: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Comic Sans',
+            ),
+            body1: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Anton'
+            ),
+            body2: TextStyle(
+                color: Colors.white70,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Lato'
+            ),
+          ),
+        ),
+        home: MyHomePage(),
+        routes: {
+          ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
+        },
       ),
-      home: MyHomePage(),
     );
   }
 }
--- widgets/product_item.dart
+++ widgets/product_item.dart
@@ -1,3 +1,4 @@
+import '../screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
@@ -13,19 +14,42 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(icon: Icon(Icons.search), onPressed: () {}),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    Color primaryColor = Theme.of(context).primaryColor;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
-        trailing: IconButton(icon: Icon(Icons.add_shopping_cart), onPressed: () {}),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+            color: primaryColor,
+          ),
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.body2,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.add_shopping_cart,
+            ),
+            onPressed: () {},
+            color: primaryColor,
+          ),
+        ),
       ),
     );
   }
