5e9916cfc875014876117e9c0cafb0911e05c319
pinyana <60167177+pinyana@users.noreply.github.com>
2020-05-05 16:03:06 -0400

    Provider

--- screens/product_detail_screen.dart
+++ screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
+import '../providers/products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 /*
@@ -8,13 +10,18 @@ class ProductDetailScreen extends StatelessWidget {
   ProductDetailScreen(this.title, this.price);
 */
 
-static const routeName = '/product-detail';
+  static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments
-    as String;
-    // ...
-    return Scaffold(appBar: AppBar(title: Text('title'),),);
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context, listen: false,)
+        .findById(productId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
   }
 }
