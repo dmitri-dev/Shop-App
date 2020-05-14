a85bf0822b994c4496a69c62922ed8919b537d64
pinyana <60167177+pinyana@users.noreply.github.com>
2020-05-14 14:11:52 -0400

    Image Preview

--- screens/edit_product_screen.dart
+++ screens/edit_product_screen.dart
@@ -10,16 +10,37 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
 
   @override
   void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final _quarterDeviceWidth = MediaQuery.of(
+          context,
+        ).size.width *
+        .25;
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
@@ -50,11 +71,51 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 },
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Description'),
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                ),
                 maxLines: 3,
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: _quarterDeviceWidth,
+                    height: _quarterDeviceWidth,
+                    margin: EdgeInsets.only(
+                      top: _quarterDeviceWidth * .2,
+                      right: _quarterDeviceWidth * .25,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Image URL',
+                      ),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
